a46e4a656db797223472480f7e96d06905340fa5
Flash-Original <40142704+Flash-Original@users.noreply.github.com>
2019-05-14 20:19:54 +0200

    Update ejercicio 2.1.3.3.cpp

--- FUNDAMENTOS C/Tema2/ejercicio 2.1.3.3.cpp
+++ FUNDAMENTOS C/Tema2/ejercicio 2.1.3.3.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*Ejercicio 2.1.3.3 Si suponemos que una canción tipica en formato MP3 ocupa cerca de 3.500kb
-¿cuántas se podrían guardar en un reproductor MP3 que tenga 256 Mb de capacidad?*/
+/*Ejercicio 2.1.3.3 Si suponemos que una canciÃ³n tipica en formato MP3 ocupa cerca de 3.500kb
+Â¿cuÃ¡ntas se podrÃ­an guardar en un reproductor MP3 que tenga 256 Mb de capacidad?*/
 
 int main(){
 	float x=3500,MP3=256,res;
-	res=256/(3500/1024);
+	res=256*1024/3500;
 printf("El resultado es: %.0f",res);
 
 return 0;	
